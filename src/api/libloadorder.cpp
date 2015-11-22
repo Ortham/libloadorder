dfb6c9055de46e5ab24c30b6177bfc5b056eb117
Oliver Hamlet <oliver.hamlet@gmail.com>
2015-11-22 17:30:06 +0000

    Bump major version to 7
    This closes #20.

--- src/api/libloadorder.cpp
+++ src/api/libloadorder.cpp
@@ -37,9 +37,9 @@ using namespace liblo;
    Version Functions
    ------------------------------*/
 
-const unsigned int LIBLO_VERSION_MAJOR = 6;
+const unsigned int LIBLO_VERSION_MAJOR = 7;
 const unsigned int LIBLO_VERSION_MINOR = 0;
-const unsigned int LIBLO_VERSION_PATCH = 3;
+const unsigned int LIBLO_VERSION_PATCH = 0;
 
 /* Returns whether this version of libloadorder is compatible with the given
    version of libloadorder. */
