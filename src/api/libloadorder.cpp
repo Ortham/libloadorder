91fc7bfb07d69d6513ba30f31e5dc7a690217350
Oliver Hamlet <oliver.hamlet@gmail.com>
2016-11-12 08:44:23 +0000

    Increment version to 9.5.3

--- src/api/libloadorder.cpp
+++ src/api/libloadorder.cpp
@@ -39,7 +39,7 @@ using namespace liblo;
 
 const unsigned int LIBLO_VERSION_MAJOR = 9;
 const unsigned int LIBLO_VERSION_MINOR = 5;
-const unsigned int LIBLO_VERSION_PATCH = 2;
+const unsigned int LIBLO_VERSION_PATCH = 3;
 
 /* Returns whether this version of libloadorder is compatible with the given
    version of libloadorder. */
