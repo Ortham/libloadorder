Ortham/libloadorder

commit adf159866aea009a055781e15c434ab9640486ae

Improve the performance of saving load orders

src/load_order/asterisk_based.rs
@@ -17,7 +17,7 @@
  * along with libloadorder. If not, see <http://www.gnu.org/licenses/>.
  */
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufWriter, Write};
 
 use encoding::{Encoding, EncoderTrap};
 use encoding::all::WINDOWS_1252;
@@ -106,19 +106,25 @@ impl WritableLoadOrder for AsteriskBasedLoadOrder {
     fn save(&mut self) -> Result<(), Error> {
         create_parent_dirs(self.game_settings().active_plugins_file())?;
 
-        let mut file = File::create(self.game_settings().active_plugins_file())?;
-        for plugin_name in self.plugin_names() {
-            if self.game_settings().is_implicitly_active(&plugin_name) {
+        let file = File::create(self.game_settings().active_plugins_file())?;
+        let mut writer = BufWriter::new(file);
+        for plugin in self.plugins() {
+            let name = match plugin.name() {
+                Some(x) => x,
+                None => continue,
+            };
+
+            if self.game_settings().is_implicitly_active(&name) {
                 continue;
             }
 
-            if self.is_active(&plugin_name) {
-                write!(file, "*")?;
+            if plugin.is_active() {
+                write!(writer, "*")?;
             }
-            file.write_all(&WINDOWS_1252
-                .encode(&plugin_name, EncoderTrap::Strict)
+            writer.write_all(&WINDOWS_1252
+                .encode(&name, EncoderTrap::Strict)
                 .map_err(Error::EncodeError)?)?;
-            writeln!(file, "")?;
+            writeln!(writer, "")?;
         }
 
         Ok(())

src/load_order/textfile_based.rs
@@ -17,7 +17,7 @@
  * along with libloadorder. If not, see <http://www.gnu.org/licenses/>.
  */
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 
 use encoding::{Encoding, EncoderTrap};
@@ -186,9 +186,10 @@ impl TextfileBasedLoadOrder {
         if let Some(file_path) = self.game_settings().load_order_file() {
             create_parent_dirs(file_path)?;
 
-            let mut file = File::create(file_path)?;
+            let file = File::create(file_path)?;
+            let mut writer = BufWriter::new(file);
             for plugin_name in self.plugin_names() {
-                writeln!(file, "{}", plugin_name)?;
+                writeln!(writer, "{}", plugin_name)?;
             }
         }
         Ok(())
@@ -197,12 +198,13 @@ impl TextfileBasedLoadOrder {
     fn save_active_plugins(&self) -> Result<(), Error> {
         create_parent_dirs(self.game_settings().active_plugins_file())?;
 
-        let mut file = File::create(self.game_settings().active_plugins_file())?;
+        let file = File::create(self.game_settings().active_plugins_file())?;
+        let mut writer = BufWriter::new(file);
         for plugin_name in self.active_plugin_names() {
-            file.write_all(&WINDOWS_1252
+            writer.write_all(&WINDOWS_1252
                 .encode(&plugin_name, EncoderTrap::Strict)
                 .map_err(Error::EncodeError)?)?;
-            writeln!(file, "")?;
+            writeln!(writer, "")?;
         }
 
         Ok(())

src/load_order/timestamp_based.rs
@@ -19,7 +19,7 @@
 use std::cmp::Ordering;
 use std::collections::BTreeSet;
 use std::fs::File;
-use std::io::{BufReader, BufRead, Write};
+use std::io::{BufReader, BufRead, BufWriter, Write};
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use encoding::{Encoding, EncoderTrap};
@@ -169,16 +169,17 @@ fn save_active_plugins<T: MutableLoadOrder>(load_order: &mut T) -> Result<(), Er
 
     let prelude = get_file_prelude(load_order.game_settings())?;
 
-    let mut file = File::create(&load_order.game_settings().active_plugins_file())?;
-    file.write_all(&prelude)?;
+    let file = File::create(&load_order.game_settings().active_plugins_file())?;
+    let mut writer = BufWriter::new(file);
+    writer.write_all(&prelude)?;
     for (index, plugin_name) in load_order.active_plugin_names().iter().enumerate() {
         if load_order.game_settings().id() == GameId::Morrowind {
-            write!(file, "GameFile{}=", index)?;
+            write!(writer, "GameFile{}=", index)?;
         }
-        file.write_all(&WINDOWS_1252
+        writer.write_all(&WINDOWS_1252
             .encode(plugin_name, EncoderTrap::Strict)
             .map_err(Error::EncodeError)?)?;
-        writeln!(file, "")?;
+        writeln!(writer, "")?;
     }
 
     Ok(())

commit 4f3b672f843b0284e58e04d94863b2f1f6ffe294

Fix writing ghosted plugins to plugins.txt
It was broken in adf159866aea009a055781e15c434ab9640486ae.

src/load_order/asterisk_based.rs
@@ -109,7 +109,7 @@ impl WritableLoadOrder for AsteriskBasedLoadOrder {
         let file = File::create(self.game_settings().active_plugins_file())?;
         let mut writer = BufWriter::new(file);
         for plugin in self.plugins() {
-            let name = match plugin.name() {
+            let name = match plugin.unghosted_name() {
                 Some(x) => x,
                 None => continue,
             };
@@ -180,6 +180,8 @@ mod tests {
     use super::*;
 
     use std::fs::{File, remove_dir_all};
+    use std::io;
+    use std::io::{BufRead, BufReader};
     use std::path::Path;
     use filetime::{FileTime, set_file_times};
     use tempdir::TempDir;
@@ -604,6 +606,36 @@ mod tests {
         assert_eq!(
             vec!["Skyrim.esm", "Blank.esp"],
             load_order.active_plugin_names()
+        );
+    }
+
+    #[test]
+    fn save_should_write_unghosted_plugin_names() {
+        let tmp_dir = TempDir::new("libloadorder_test_").unwrap();
+        let mut load_order = prepare(GameId::SkyrimSE, &tmp_dir.path());
+
+        copy_to_test_dir(
+            "Blank - Different.esm",
+            "ghosted.esm.ghost",
+            &load_order.game_settings(),
+        );
+        let plugin = Plugin::new("ghosted.esm.ghost", &load_order.game_settings()).unwrap();
+        load_order.plugins_mut().push(plugin);
+
+        load_order.save().unwrap();
+
+        let reader = BufReader::new(
+            File::open(load_order.game_settings().active_plugins_file()).unwrap(),
+        );
+
+        let lines = reader
+            .lines()
+            .collect::<Result<Vec<String>, io::Error>>()
+            .unwrap();
+
+        assert_eq!(
+            vec!["*Blank.esp", "Blank - Different.esp", "ghosted.esm"],
+            lines
         );
     }
 