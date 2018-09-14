4926b6bb03d57dff414c6a7e4ec7d667c620d4fd
Oliver Hamlet <oliver.hamlet@gmail.com>
2018-09-14 21:45:37 +0100

    Rework ReadableLoadOrder tests
    Test an implementation of ReadableLoadOrderExt to simplify extending
    coverage in the future.

--- src/load_order/readable.rs
+++ src/load_order/readable.rs
@@ -254,41 +254,83 @@ mod tests {
     use load_order::tests::mock_game_files;
     use tests::copy_to_test_dir;
 
-    fn prepare(game_dir: &Path) -> Vec<Plugin> {
-        let (_, plugins) = mock_game_files(GameId::Oblivion, game_dir);
-
-        plugins
+    struct TestLoadOrder {
+        game_settings: GameSettings,
+        plugins: Vec<Plugin>,
     }
 
-    fn prepare_with_ghosted_plugin(game_dir: &Path) -> Vec<Plugin> {
-        let (settings, mut plugins) = mock_game_files(GameId::Oblivion, game_dir);
+    impl ReadableLoadOrder for TestLoadOrder {
+        fn game_settings(&self) -> &GameSettings {
+            &self.game_settings
+        }
+
+        fn plugin_names(&self) -> Vec<&str> {
+            plugin_names(&self.plugins)
+        }
+
+        fn index_of(&self, plugin_name: &str) -> Option<usize> {
+            index_of(&self.plugins, plugin_name)
+        }
+
+        fn plugin_at(&self, index: usize) -> Option<&str> {
+            plugin_at(&self.plugins, index)
+        }
+
+        fn active_plugin_names(&self) -> Vec<&str> {
+            active_plugin_names(&self.plugins)
+        }
+
+        fn is_active(&self, plugin_name: &str) -> bool {
+            is_active(&self.plugins, plugin_name)
+        }
+    }
+
+    impl ReadableLoadOrderExt for TestLoadOrder {
+        fn plugins(&self) -> &Vec<Plugin> {
+            &self.plugins
+        }
+    }
+
+    fn prepare(game_dir: &Path) -> TestLoadOrder {
+        let (game_settings, plugins) = mock_game_files(GameId::Oblivion, game_dir);
+        TestLoadOrder {
+            game_settings,
+            plugins,
+        }
+    }
+
+    fn prepare_with_ghosted_plugin(game_dir: &Path) -> TestLoadOrder {
+        let (game_settings, mut plugins) = mock_game_files(GameId::Oblivion, game_dir);
 
         copy_to_test_dir(
             "Blank - Different.esm",
             "Blank - Different.esm.ghost",
-            &settings,
+            &game_settings,
         );
         plugins.insert(
             1,
-            Plugin::new("Blank - Different.esm.ghost", &settings).unwrap(),
+            Plugin::new("Blank - Different.esm.ghost", &game_settings).unwrap(),
         );
 
-        plugins
+        TestLoadOrder {
+            game_settings,
+            plugins,
+        }
     }
 
     #[test]
     fn plugin_names_should_return_filenames_for_plugins_in_load_order() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
         let expected_plugin_names = vec!["Oblivion.esm", "Blank.esp", "Blank - Different.esp"];
-        assert_eq!(expected_plugin_names, plugin_names(&plugins));
+        assert_eq!(expected_plugin_names, load_order.plugin_names());
     }
 
     #[test]
     fn plugin_names_should_return_unghosted_filenames() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare_with_ghosted_plugin(&tmp_dir.path());
+        let load_order = prepare_with_ghosted_plugin(&tmp_dir.path());
 
         let expected_plugin_names = vec![
             "Oblivion.esm",
@@ -296,95 +338,95 @@ mod tests {
             "Blank.esp",
             "Blank - Different.esp",
         ];
-        assert_eq!(expected_plugin_names, plugin_names(&plugins));
+        assert_eq!(expected_plugin_names, load_order.plugin_names());
     }
 
     #[test]
     fn index_of_should_return_none_if_the_plugin_is_not_in_the_load_order() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert!(index_of(&plugins, "Blank.esm").is_none());
+        assert!(load_order.index_of("Blank.esm").is_none());
     }
 
     #[test]
     fn index_of_should_return_some_index_if_the_plugin_is_in_the_load_order() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert_eq!(1, index_of(&plugins, "Blank.esp").unwrap());
+        assert_eq!(1, load_order.index_of("Blank.esp").unwrap());
     }
 
     #[test]
     fn index_of_should_be_case_insensitive() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert_eq!(1, index_of(&plugins, "blank.esp").unwrap());
+        assert_eq!(1, load_order.index_of("blank.esp").unwrap());
     }
 
     #[test]
     fn plugin_at_should_return_none_if_given_an_out_of_bounds_index() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert!(plugin_at(&plugins, 3).is_none());
+        assert!(load_order.plugin_at(3).is_none());
     }
 
     #[test]
     fn plugin_at_should_return_some_filename_if_given_an_in_bounds_index() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert_eq!("Blank.esp", plugin_at(&plugins, 1).unwrap());
+        assert_eq!("Blank.esp", load_order.plugin_at(1).unwrap());
     }
 
     #[test]
     fn plugin_at_should_return_some_unghosted_filename() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare_with_ghosted_plugin(&tmp_dir.path());
+        let load_order = prepare_with_ghosted_plugin(&tmp_dir.path());
 
-        assert_eq!("Blank - Different.esm", plugin_at(&plugins, 1).unwrap());
+        assert_eq!("Blank - Different.esm", load_order.plugin_at(1).unwrap());
     }
 
     #[test]
     fn active_plugin_names_should_return_filenames_for_active_plugins_in_load_order() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
         let expected_plugin_names = vec!["Blank.esp"];
-        assert_eq!(expected_plugin_names, active_plugin_names(&plugins));
+        assert_eq!(expected_plugin_names, load_order.active_plugin_names());
     }
 
     #[test]
     fn is_active_should_return_false_for_an_inactive_plugin() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert!(!is_active(&plugins, "Blank - Different.esp"));
+        assert!(!load_order.is_active("Blank - Different.esp"));
     }
 
     #[test]
     fn is_active_should_return_false_a_plugin_not_in_the_load_order() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert!(!is_active(&plugins, "missing.esp"));
+        assert!(!load_order.is_active("missing.esp"));
     }
 
     #[test]
     fn is_active_should_return_true_for_an_active_plugin() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert!(is_active(&plugins, "Blank.esp"));
+        assert!(load_order.is_active("Blank.esp"));
     }
 
     #[test]
     fn is_active_should_be_case_insensitive() {
         let tmp_dir = tempdir().unwrap();
-        let plugins = prepare(&tmp_dir.path());
+        let load_order = prepare(&tmp_dir.path());
 
-        assert!(is_active(&plugins, "blank.esp"));
+        assert!(load_order.is_active("blank.esp"));
     }
 }
